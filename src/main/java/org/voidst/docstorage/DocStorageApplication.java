9f33c9ebf53caa9b45191c78e5241a83c1600982
asablin <void.st@gmail.com>
2023-01-25 10:15:07 +0300

    init

---
+++ src/main/java/org/voidst/docstorage/DocStorageApplication.java
@@ -0,0 +1,13 @@
+package org.voidst.docstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocStorageApplication.class, args);
+	}
+
+}
